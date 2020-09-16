df0ecafd16851586718de126a4b0a3289a9b22b3
DiegoMazega <diego.mazega@live.com>
2020-09-16 15:33:52 -0300

    Project created

---
+++ backend/src/main/java/com/diegomazega/pesquisajogos/PesquisajogosApplication.java
@@ -0,0 +1,13 @@
+package com.diegomazega.pesquisajogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisajogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisajogosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/diegomazega/pesquisajogos/PesquisajogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegomazega.pesquisajogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisajogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
